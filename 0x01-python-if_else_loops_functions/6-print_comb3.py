961eab2f470bf910010b9ae5b81176c7144c9eb4
monicajaimesc <you@example.com>
2019-09-11 02:34:13 +0000

    bug fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 for i in range(10):
     for j in range(10):
     # print numbers until 09
-    if i == 8 and j == 9:
-         print("{}{}".format(i, j))
-     break
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+            break
     # print numbers after 09
-    if j > i:
-        print("{}{}, ".format(i, j), end="")
+        if j > i:
+            print("{}{}, ".format(i, j), end="")
