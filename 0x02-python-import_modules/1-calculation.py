bad2b1d9b73efcda7e4bebbac3b06223f427bf0d
monicajaimesc <monikmaja@gmail.com>
2019-09-12 20:32:31 +0000

    identation fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from calculator_1 import add, sub, mul, div
-	a = 10
-	b = 5
-	c = add(a, b)
-	print("{0} + {1} = {2}".format(a, b, c))
-	c = sub(a, b)
-	print("{0} - {1} = {2}".format(a, b, c))
-	c = mul(a, b)
-	print("{0} * {1} = {2}".format(a, b, c))
-	c = div(a, b)
-	print("{0} / {1} = {2}".format(a, b, c))
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    c = add(a, b)
+    print("{0} + {1} = {2}".format(a, b, c))
+    c = sub(a, b)
+    print("{0} - {1} = {2}".format(a, b, c))
+    c = mul(a, b)
+    print("{0} * {1} = {2}".format(a, b, c))
+    c = div(a, b)
+    print("{0} / {1} = {2}".format(a, b, c))
