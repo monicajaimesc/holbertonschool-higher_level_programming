174e49638723410cfa959314c0056db947744cf0
monicajaimesc <monikmaja@gmail.com>
2019-09-12 21:52:54 +0000

    pip8 fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         for i in range(a):
             if i == 0:
                 continue
-            print("{:d}: {:s}".format(i, sys.argv[i])) 
+            print("{:d}: {:s}".format(i, sys.argv[i]))
