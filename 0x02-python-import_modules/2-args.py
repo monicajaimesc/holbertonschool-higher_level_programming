bd6d355525f6d1968163ce7e138ace99df9fd565
monicajaimesc <monikmaja@gmail.com>
2019-09-12 21:46:57 +0000

    strings with arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv)
+    if a == 1:
+        print("{:d} arguments".format(a - 1))
+    elif a == 2:
+        print("{:d} argument:".format(a - 1))
+        print("{:d}: {:s}".format(a - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(a - 1))
+        for i in range(a):
+            if i == 0:
+                continue
+            print("{:d}: {:s}".format(i, sys.argv[i])) 
