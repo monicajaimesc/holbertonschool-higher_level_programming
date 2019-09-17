d516fe4816b056f3e77755711fc718954c61f45c
monicajaimesc <monikmaja@gmail.com>
2019-09-17 18:19:25 +0000

    for changed for range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
         return None
     elif idx >= len(my_list):
         return None
-    for i in my_list:
+    for i in range(len( my_list)):
         if idx == i:
             return my_list[i]
