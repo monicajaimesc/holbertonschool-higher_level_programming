00910d39669375e169045b71c2283cdadec9d2fb
monicajaimesc <monikmaja@gmail.com>
2019-09-17 22:37:55 +0000

    fixing bugs

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None:
-        return None
-    copy_list = my_list[:]
+    copy_list = my_list.copy
     if idx < 0:
         return copy_list
     if idx >= len(my_list):
