26e71fcd47f6cd211fd637698213a9fe8fc152eb
monicajaimesc <monikmaja@gmail.com>
2019-09-17 19:39:39 +0000

    function that replace an element in a list without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_list = my_list[:]
+    if idx < 0:
+        return copy_list
+    if idx >= len(my_list):
+        return copy_list
+    my_list[idx] = element
+    return copy_list
