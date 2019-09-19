4a29f0df6086bc56bee6c49d112f4089e0e4f421
monicajaimesc <monikmaja@gmail.com>
2019-09-19 22:47:59 +0000

    find a common element in two list

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common_element = set_1.intersection(set_2)
+    return common_element
+
+    # return set_1 & set_2
+
+    '''
+    Intersection of two list means we need to
+    take all those elements which are common to
+    both of the initial lists and store them into
+    another list.
+    '''
