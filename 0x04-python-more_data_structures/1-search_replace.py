0e074cc01d51a925fabd5f22d39e71cf56e6a81f
monicajaimesc <monikmaja@gmail.com>
2019-09-19 21:09:59 +0000

    function that replaces all occurences of an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,9 +3,12 @@ def search_replace(my_list, search, replace):
     copy_list = my_list[:]
     for digit in range(len(copy_list)):
         if copy_list[digit] == search:
-            copy_list[digit] == replace
+            # had a mistake, was using == instead of =
+            # is an assignment not equal
+            copy_list[digit] = replace
 
     return copy_list
 # copy_list = list(map(lambda digit:
 # replace if digit == search else digit, my_list))
 # return copy_list
+# replace will return copy of the list
