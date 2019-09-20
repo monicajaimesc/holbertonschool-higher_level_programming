f0933f1bb748c391df0879d49cdfb0db34b0e5da
monicajaimesc <monikmaja@gmail.com>
2019-09-20 04:16:59 +0000

    letter in a or b but not both

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # 1) x1 | x2
-    # 2)
-    return[(set_1.union(set_2))]
+    # letters in a or b but not both
+    return (set_1 ^ (set_2))
