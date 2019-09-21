722610c1e1363665a208c1b3ec772a14bea3e40a
monicajaimesc <monikmaja@gmail.com>
2019-09-21 20:04:27 +0000

    bug fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        # value is an integer
-        key_value = sorted(a_dictionary.key_value())
-        result = key_value[-1]
-        for key_value in a_dictionary:
-            if result == a_dictionary[key_value]:
-                return key_value
+        # values are integers
+        values = sorted(a_dictionary.values())
+        result = values[-1]
+        for best_key in a_dictionary:
+            if result == a_dictionary[best_key]:
+                return best_key
+
+
+'''
+other form:
+if max(a_dictionary, key=a_dictionary.get) is 0:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
+'''
