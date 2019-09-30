5308aa4a641fe4c28ceecb69fa7c056bf0b16b62
monicajaimesc <monikmaja@gmail.com>
2019-09-30 17:27:04 +0000

    one return deleted

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         print()
+        return x
     except IndexError:
         print()
         return (i)
-# x can be bigger than the length of my_list
-    return (i + 1)
