a1de1a52bb14cf4b1a70073f3dc7e94d8c496237
monicajaimesc <monikmaja@gmail.com>
2019-09-30 17:49:10 +0000

    safre print list integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    try:
-        for i in range(x):
-            if type(my_list[i]) is int:
-                print("{}".format(my_list[i]), end="")
-                counter += 1
-        print()
-    except IndexError:
-        raise
-    # x can be bigger than the length of my_list
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        # specting integer, sending string
+        except ValueError:
+            continue
+        #  an operation is not soported
+        except TypeError:
+            continue
+
+    print()
     return (counter)
