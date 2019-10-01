19e448c765bf08a14cc40c2bf80b1730aa5e00ed
monicajaimesc <monikmaja@gmail.com>
2019-10-01 21:26:42 +0000

    function for an empty square and for an empty square with an instance

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+square Module - {} for all your needs if they include printing squares
+"""
+
+
+class Square:
+    """
+    First pass at creating a square class
+    """
+    pass
+
+    # pass is a place holder (;keywordC/py)
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+Square Module - for all your needs, as long as they include printing squares
+"""
+
+
+class Square:
+    """
+    Square class: was created
+    """
+    def __init__(self, size):
+        """
+        instance attribute: size (created), invoke with self
+        """
+        self.__size = size
