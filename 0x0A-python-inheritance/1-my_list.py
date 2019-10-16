f62a09469cc18e2b2943b6cd95d4784dad14fdac
monicajaimesc <monikmaja@gmail.com>
2019-10-16 02:52:05 +0000

    list sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usrt/bin/python3
+"""
+module for my lists (inherits form list)
+"""
+
+
+class MyList(list):
+    """
+    elements of the list int type
+    return my list and sorted list
+    """
+    def print_sorted(self):
+        # sorted method
+        # sorted(iterable[, key][, reverse])
+        print(sorted(self))
