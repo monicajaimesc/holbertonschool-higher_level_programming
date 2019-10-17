da104f331ab1b135b0963493317a471b6476b05b
monicajaimesc <monikmaja@gmail.com>
2019-10-17 03:17:23 +0000

    output fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,43 +1,8 @@
 #!/usr/bin/python3
 """
-Module for a BaseGeometry class
+Module for a rectangle Class class
 """
-
-
-class BaseGeometry:
-    """
-    still empty, area not implemented
-    """
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    rectangle clasee herencia from BaseGeometry
-    """
-    def __init__(self, width, height):
-        """
-        init constructor
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """ returns the rectangle area """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """ string with the following description """
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -46,5 +11,11 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         """ constructor, init square """
-        self.__size = size
-        super().__init__(self.__size, self.__size)
+        if self.integer_validator("size", size) is None:
+            self.__size = size
+
+        super().__init__(size, size)
+
+    def area(self):
+        """ returns the rectangle area """
+        return (self.__size * self.__size)
