6d41a8e8c18e8648bb9ca8d4c257086531b5da6c
monicajaimesc <monikmaja@gmail.com>
2019-10-17 02:33:13 +0000

    student class with reload method

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that contain a class Student
+"""
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return studen dictionary """
+        temporal_dict = {}
+        if attrs is None:
+            return self.__dict__
+        temporal_dict = dict()
+        # only attribute names
+        # contained in this list must be retrieved
+        for key in attrs:
+            if key in self.__dict__.keys():
+                temporal_dict[key] = self.__dict__[key]
+        return temporal_dict
+
+    def reload_from_json(self, json):
+        """
+        public method that replaces all
+        attributes of the Student instance
+        """
+        return self.__dict__.update(json)
