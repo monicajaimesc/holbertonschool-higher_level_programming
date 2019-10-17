6064f73419c7061367b37e06d7400c700712b428
monicajaimesc <monikmaja@gmail.com>
2019-10-17 02:36:02 +0000

    pycodestyle done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-Module for read_file, function that reads 
+Module for read_file, function that reads
 a text file (UTF8) and prints it to stdout
 """
+
 
 def read_file(filename=""):
     """
     read_file: read a file using
     with statement
     """
-with open("my_file_0.txt", mode='r' , encoding="utf-8") as f:
+
+
+with open("my_file_0.txt", mode='r', encoding="utf-8") as f:
     read_data = f.read()
 print("{:s}".format(read_data), end="")
 f.closed
