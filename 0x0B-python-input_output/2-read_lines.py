56e92535bcb996834a75052d940fb925c3a2ddbe
monicajaimesc <monikmaja@gmail.com>
2019-10-18 16:58:16 +0000

    one end missed

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -12,7 +12,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, mode='r', encoding="utf-8") as f:
         #  nb_lines is lower or equal to 0
         if nb_lines <= 0:
-            print(f.read())
+            print(f.read(), end="")
         else:
             for i in range(nb_lines):
                 print(f.readline(), end="")
