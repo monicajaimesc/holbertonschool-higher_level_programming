2570c80c9acfd12245e7da8a861c37138bb37536
monicajaimesc <monikmaja@gmail.com>
2019-10-18 16:50:39 +0000

    mistake in the open function, fixed

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -9,7 +9,7 @@ def read_lines(filename="", nb_lines=0):
     """
     return lines readed
     """
-    with open("my_file_0.txt", mode='r', encoding="utf-8") as f:
+    with open(filename, mode='r', encoding="utf-8") as f:
         #  nb_lines is lower or equal to 0
         if nb_lines <= 0:
             print(f.read())
