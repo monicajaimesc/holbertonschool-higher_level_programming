92a3b3cb8b65ea0e9efc064fb57f0083abd1401e
monicajaimesc <monikmaja@gmail.com>
2019-11-27 01:47:25 +0000

    asc deleted

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM states
-INNER JOIN cities WHERE states.id = cities.states_id
-ORDER BY cities.id ASC;
+INNER JOIN cities WHERE states.id = cities.states_id;
