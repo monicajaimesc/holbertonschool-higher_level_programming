a9cb19d1ee5353a30bb1c0811b1ca482f0c6b721
monicajaimesc <monikmaja@gmail.com>
2019-11-27 01:52:14 +0000

    inner deleted

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM states
-INNER JOIN cities WHERE states.id = cities.states_id;
+JOIN cities WHERE states.id = cities.states_id;
