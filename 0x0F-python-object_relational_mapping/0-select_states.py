a6c7248b7bf7fe11a60f63d9caa6fc5ee6939616
monicajaimesc <monikmaja@gmail.com>
2019-12-13 17:48:55 +0000

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,17 +12,15 @@ if __name__ == "__main__":
 
     # connection to the database to use
     # the parameters are passed to the Python extension _mysql.
-    db = MySQLdb.connect(host='localhost', user='username',
-                         passwd='password', db='database_name')
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database_name)
     # Getting a cursor in MySQL python
     # it gives the ability to have multiple separete working environments through
     # the same connection to the database
     cur = db.cursor()
     # the execution function requires 1 parameter, the query
-    cur.execution("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     # after execute any select statement, select one method
-
-    line = c.fetchone()
-    while (line):
-        print(line)
-        line = c.fetchone()
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
