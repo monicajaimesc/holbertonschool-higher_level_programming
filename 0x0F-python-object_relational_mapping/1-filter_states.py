63c4fb30632119c17356a4cfa646aa63128edc6f
monicajaimesc <monikmaja@gmail.com>
2019-12-14 12:53:50 -0500

    casse sensitive made it

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=database_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' "
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
                 "ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
