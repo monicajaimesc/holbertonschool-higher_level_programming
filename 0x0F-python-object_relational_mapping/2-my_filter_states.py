f0554aa48edf8bec7a381ab991aebdb4fc978404
monicajaimesc <monikmaja@gmail.com>
2019-12-14 13:11:12 -0500

    order missed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     # like or = is the same
     cur.execute("SELECT * FROM states "
-                "WHERE name LIKE BYNARY '{:s}'".format(state_searched))
+                "WHERE name LIKE '{:s}'"
+                "ORDER BY states.id ASC".format(state_searched))
     rows = cur.fetchall()
     for row in rows:
         print(row)
