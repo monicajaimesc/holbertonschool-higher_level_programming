57fef1160a84c1f35da1cfbc055f59561ad90a5b
monicajaimesc <47387691+monicajaimesc@users.noreply.github.com>
2019-12-15 22:46:46 -0500

    delete

--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
-
