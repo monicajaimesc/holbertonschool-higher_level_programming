1993a77c91b6a421b80056350c042a306fb65ad5
monicajaimesc <monikmaja@gmail.com>
2019-12-16 02:55:16 +0000

    order by modificated

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).\
-        order_by(State.id).all()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
     if not state:
         print("Not found")
         exit
