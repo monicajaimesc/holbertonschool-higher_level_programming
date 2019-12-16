22230e9dc9f336f7c8d4e61c67a4e4366526bb77
monicajaimesc <monikmaja@gmail.com>
2019-12-16 04:05:17 +0000

    execute fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,7 +2,6 @@
 """
 lists all cities from the database
 """
-from machine.programs.objects.AirBnB_clone_v3.api.v1.views import states
 import MySQLdb
 import sys
 if __name__ == "__main__":
@@ -15,11 +14,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=database_name)
     cur = db.cursor()
-
-    cur.execute("SELECT cities.id, cities.name, states.name "
-                "FROM cities JOIN states "
-                "ON cities.state_id = states.id "
-                "ORDER BY cities.id")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN\
+                states ON cities.state_id = states.id ORDER BY cities.id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
