0fecb28e4299852c3d33f7a0176941cc5e39d0d2
monicajaimesc <monikmaja@gmail.com>
2019-12-16 03:42:48 +0000

    fixing bug

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,13 +10,16 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=database_name)
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN\
-                states ON cities.state_id = states.id ORDER BY cities.id;")
-    result = cur.fetchall()
-    for row in result:
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities JOIN states "
+                "ON cities.state_id = states.id "
+                "ORDER BY cities.id")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
