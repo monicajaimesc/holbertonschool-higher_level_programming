77d77df5714b06ef6a32901a6061c3a016e634c4
monicajaimesc <monikmaja@gmail.com>
2020-01-06 21:54:18 +0000

    permissions

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
+# takes in a URL, sends a GET request to the URL and displays the body of the response
 curl -s -L -X GET "$1"
