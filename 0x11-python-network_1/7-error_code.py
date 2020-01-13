bafec104bc759d4c9db47648ebc14304313a514e
monicajaimesc <monikmaja@gmail.com>
2020-01-13 22:09:18 +0000

    error code greater or equal to 400

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# takes in a URL, sends a request to the URL and displays the body response
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    parameter = sys.argv[1]
+    # requests provide access to the full range of HTTP verbs:
+    # Get in this case
+    response = requests.get(parameter)
+    if response.status_code >= 400:
+        print('Error code: {}'.format(response.status_code))
+    else:
+        print(response.text)
