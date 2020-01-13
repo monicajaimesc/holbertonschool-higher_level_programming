7d8b39fab30738a0a226311662cfd0bd603f75b5
monicajaimesc <monikmaja@gmail.com>
2020-01-13 19:57:56 +0000

    requiriment fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# takes a URL and an email, sends a POST request to the URL with the email as a parameter
+# takes a URL and an email sends a POST request to the URL
 
 import sys
 import urllib.request
@@ -10,10 +10,9 @@ if "__main__" == __name__:
     parameter1 = sys.argv[1]
     parameter2 = sys.argv[2]
     values = {'email': parameter2}
-    email = urllib.parse.urlencode(values).encode('ascii')
+    data = urllib.parse.urlencode(values).encode('ascii')
     # sends a POST request to the URL as parameter
-    with urllib.request.urlopen(parameter1, email) as status:
+    with urllib.request.urlopen(parameter1, data) as status:
         text = status.read()
         decode_text = text.decode('utf-8')
         print(decode_text)
-
