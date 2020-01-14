fc4d1ca8a79659675d82b838bf509c4fe25cddc3
monicajaimesc <monikmaja@gmail.com>
2020-01-14 02:19:48 +0000

    auth added

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     parameter1 = sys.argv[1]
     parameter2 = sys.argv[2]
-    response = requests.get('https://api.github.com/user'
-                            .format(parameter1, parameter2))
+    response = requests.get('https://api.github.com/user',
+                            auth=(parameter1, parameter2))
     decode = response.json()
     print(decode.get('id'))
