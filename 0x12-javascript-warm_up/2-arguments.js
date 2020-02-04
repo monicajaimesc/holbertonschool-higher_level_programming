4a3895b6888460416eab3fb02891b76394fb5dcb
monicajaimesc <monikmaja@gmail.com>
2020-02-04 16:08:09 -0500

    checker didnt accept it, last form

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv[3]) {
-  console.log('Arguments found')
-} else if (process.argv.length === 2) {
-  console.log('Argument found')
+  console.log('Arguments found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
 } else {
-  console.log('No argument')
+  console.log('No argument');
 }
