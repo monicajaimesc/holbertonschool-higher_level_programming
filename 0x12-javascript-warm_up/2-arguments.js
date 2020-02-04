f29b9bff4a024b8e8d7fc8d4d0e51f1f91a86a75
monicajaimesc <monikmaja@gmail.com>
2020-02-04 15:42:44 -0500

    requeriment fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const a = 'Arguments found'
+const c = 'Arguments found'
 const b = 'No argument'
 
 if (process.argv[3]) {
   console.log(a)
 } else if (process.argv[2]) {
-  console.log(a)
+  console.log(c)
 } else {
   console.log(b)
 }
