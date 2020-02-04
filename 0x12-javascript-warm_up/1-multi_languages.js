23cabe7c5176e89781d64bd88105b5b7622348d5
monicajaimesc <monikmaja@gmail.com>
2020-02-04 14:43:07 -0500

    semicolon added

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing'
-console.log(myVar)
+const text = 'C is fun\nPython is cool\nJavascript is amazing'
+console.log(text);
