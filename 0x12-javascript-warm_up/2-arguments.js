5e599c7dbf2a22907212e02fc86e7e297fbef2fb
monicajaimesc <monikmaja@gmail.com>
2020-02-04 15:40:51 -0500

    standar ready

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let a = 'Arguments found';
-let b = 'No argument';
+const a = 'Arguments found'
+const b = 'No argument'
 
-if (process.argv[3]){
-    console.log(a);
+if (process.argv[3]) {
+  console.log(a)
 } else if (process.argv[2]) {
-    console.log(a);
+  console.log(a)
 } else {
-    console.log(b);
+  console.log(b)
 }
