bd7c454af5f8a67608cff339f51b6778e9cb4ab7
monicajaimesc <monikmaja@gmail.com>
2020-02-04 19:49:29 -0500

    i missed a semicolon

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@ const decimalnumber = parseInt(process.argv[2]);
 if (ilegalnumber) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + decimalnumber)
+  console.log('My number: ' + decimalnumber);
 }
