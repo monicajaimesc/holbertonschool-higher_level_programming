0ff6d9f6766e75e64cc16f774bff6b3e016b0aa7
monicajaimesc <monikmaja@gmail.com>
2020-02-05 01:09:38 +0000

    missing one ; again

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing']
 
 for (let current = 0; current < lines.length; current++) {
-  console.log(lines[current])
+  console.log(lines[current]);
 }
