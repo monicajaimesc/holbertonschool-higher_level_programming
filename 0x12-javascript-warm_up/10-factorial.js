0552755dab103d171ce063cf8d9857348901934a
monicajaimesc <monikmaja@gmail.com>
2020-02-05 02:55:49 +0000

    factorial done

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const digit = parseInt(process.argv[2]);
+
+function factorial (number) {
+  if (!number) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1))
+  }
+}
+console.log(factorial(digit));
