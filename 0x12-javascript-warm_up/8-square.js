ab1e9a98b8b2d663adbcfad0d28355342971c2af
monicajaimesc <monikmaja@gmail.com>
2020-02-05 02:29:39 +0000

    if changed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const args = process.argv[2];
 const x = parseInt(process.argv[2]);
 let current = 0;
 
-if (parseInt(args)) {
+if (!args || !x) {
   console.log('Missing number of occurrences');
 } else {
   while (current < x) {
