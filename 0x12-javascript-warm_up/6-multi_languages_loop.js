4bc753f21dd17511922a5faeef2480e6d43516f4
monicajaimesc <monikmaja@gmail.com>
2020-02-05 01:08:23 +0000

    extra lines, string in single quotes, miseed brackets

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-let lines = ["C is fun", "Python is cool", "Javascript is amazing"];
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing']
 
-for (let current = 0; current < lines.length; current++)
-  console.log(lines[current]);
+for (let current = 0; current < lines.length; current++) {
+  console.log(lines[current])
+}
