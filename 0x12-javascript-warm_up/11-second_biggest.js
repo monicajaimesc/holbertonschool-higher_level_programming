c1b491bcf9a001368a3b22da167b35f40572480c
monicajaimesc <monikmaja@gmail.com>
2020-02-05 23:43:05 +0000

    output done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,10 @@ const args = process.argv;
 if (!process.argv[3]) {
   console.log(0);
 } else {
-  console.log(args.sort().slice(-2, -1).toString());
+  args.sort();
+  let new_args = args.filter((item, index) => args.indexOf(item) === index);
+  new_args.shift();
+  new_args.shift();
+  new_args.reverse();
+  console.log(new_args[1]);
 }
