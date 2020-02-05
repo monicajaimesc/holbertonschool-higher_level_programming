9e7a2a5190f34cf13c616ff107254fde85f50ec7
monicajaimesc <monikmaja@gmail.com>
2020-02-05 02:32:10 +0000

    typo mistake fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,9 @@ const args = process.argv[2];
 const x = parseInt(process.argv[2]);
 let current = 0;
 
+
 if (!args || !x) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   while (current < x) {
     console.log('X'.repeat(x));
