a91d099bb6e5514b1e7e8c1efd839474dd277584
monicajaimesc <monikmaja@gmail.com>
2020-02-05 19:13:51 -0500

    more more semicolon missed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
 if (!process.argv[3]) {
-  console.log(0);
+  console.log(0)
 } else {
-  args.sort(function (a, b) { return a - b });
-  const newArgs = args.filter((item, index) => args.indexOf(item) === index);
-  newArgs.shift();
-  newArgs.shift();
-  newArgs.reverse();
-  console.log(newArgs[1]);
+  args.sort(function (a, b) { return a - b })
+  const newArgs = args.filter((item, index) => args.indexOf(item) === index)
+  newArgs.shift()
+  newArgs.shift()
+  newArgs.reverse()
+  console.log(newArgs[1])
 }
