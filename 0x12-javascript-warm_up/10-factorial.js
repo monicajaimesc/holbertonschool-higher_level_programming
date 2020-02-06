79b4d7bfc38986554cf8b512723e76454cd77201
monicajaimesc <monikmaja@gmail.com>
2020-02-06 00:07:50 +0000

    semicolon added

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ function factorial (number) {
   if (!number) {
     return (1);
   } else {
-    return (number * factorial(number - 1))
+    return (number * factorial(number - 1));
   }
 }
 console.log(factorial(digit));
