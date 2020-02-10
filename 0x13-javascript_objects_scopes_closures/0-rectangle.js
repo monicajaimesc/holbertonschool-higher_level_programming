7359907c511ca0d91d27471344b4a8a4be07697f
monicajaimesc <monikmaja@gmail.com>
2020-02-10 17:32:45 +0000

    0 rectangle is done

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+class Rectangle {
+
+}
+// module is a variable that represents current module
+// and exports is an object that will be exposed as a module
+module.exports = Rectangle;
