a90365e2fb62d8a2b7728faceed690c168b4fbed
monicajaimesc <monikmaja@gmail.com>
2020-02-10 21:09:15 +0000

    while changed for a for

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,10 +4,7 @@ Square.prototype.charPrint = function (c) {
   if (c === undefined) {
     c = 'X';
   }
-  let index = 0;
-  while (index < this.width) {
-    index++;
-    // for (let index = 0; index < this.width; index++) {
+  for (let index = 0; index < this.width; index++) {
     console.log(c.repeat(this.height));
   }
 };
