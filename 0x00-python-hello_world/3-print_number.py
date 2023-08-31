319364964485f7abf2f6dee8c75ac54a8a0a2223
Monica Jaimes <monikmaja@gmail.com>
2023-08-31 17:31:02 +0200

    actualization

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 number = 98
 print("{0:d} {1}".format(number, "Battery street"))
+
+# new format 
+number_two = 15
+print(f"{number} Battery hola {number_two}")
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -2,3 +2,7 @@
 str = "Holberton School"
 print("{}{}{}".format(str, str, str))
 print(str[0:9])
+# new format
+str_new = "nothing more"
+print(f"{str} {str_new}")
+print(str_new[:8])
